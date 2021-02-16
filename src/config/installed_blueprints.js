ade272054f185d2bd0788893ade7bc59f6440d74
TAN DUONG <tanduong@live.com>
2021-02-16 12:51:12 -0600

    rn-web build (ignore)

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -4,7 +4,12 @@ export const installed_blueprints = [
   // access route is the route nate given to navigator
   {
     name: "ImageCardList017434",
-    human_name: "ImageCardList017434",
+    human_name: "Item 1",
+    access_route: "ImageCardList017434"
+  },
+  {
+    name: "ImageCardList017434",
+    human_name: "Item 2",
     access_route: "ImageCardList017434"
   }
 ]
